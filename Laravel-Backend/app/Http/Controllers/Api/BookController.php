da6d63b2a0546e96ea6a49e4fdeafbb245c6f0ba
Thushara Thiwanka <thusharathiwanka01@gmail.com>
2022-07-29 01:57:30 +0530

    Book Controller with all CRUD Done

---
+++ Laravel-Backend/app/Http/Controllers/Api/BookController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Book;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class BookController extends Controller
+{
+    protected $book;
+
+    public function __construct(Book $book)
+    {
+        $this->book = $book;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response($this->book->all(), Response::HTTP_OK);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'genre' => 'required',
+            'price' => 'required'
+        ]);
+
+        return response($this->book->create($request->all()), Response::HTTP_CREATED);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $book = $this->book->find($id);
+
+        if (!$book) {
+            return response(['message' => 'Book not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        return response($book, Response::HTTP_OK);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $book = $this->book->find($id);
+
+        if (!$book) {
+            return response(['message' => 'Book not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        $book->update($request->all());
+
+        return response($book, Response::HTTP_OK);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $book = $this->book->find($id);
+
+        if(!$book) {
+            return response(['message' => 'Book not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        $book->delete();
+
+        return response($book, Response::HTTP_OK);
+    }
+
+    /**
+     * Search for a book by title.
+     *
+     * @param  string  $title
+     * @return \Illuminate\Http\Response
+     */
+    public function search($title)
+    {
+        return response($this->book->where('title', 'LIKE', "%$title%")->get(), Response::HTTP_OK);
+    }
+}
--- Laravel-Backend/app/Models/Book.php
+++ Laravel-Backend/app/Models/Book.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'genre',
+        'price',
+        'status',
+    ];
 }
